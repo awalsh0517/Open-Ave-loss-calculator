bc8ec3e0ea04a5c30d9fd72b2fe99c7cb9d5aa4d
DaveM <dave.miranda@gmail.com>
2020-06-15 19:59:58 -0400

    added table v4

--- components/Success.jsx
+++ components/Success.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-// import ReactTable from 'react-table'
 import viewKeys from './displayViewKeys'
 
 import { calculateSavings } from '../utils/calculations'
